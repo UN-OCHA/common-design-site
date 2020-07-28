721a56e4d3dc4b608521ebc108733863052bb245
left23 <lcdemc@gmail.com>
2020-07-28 13:56:41 +0200

    file changes after updates

--- html/modules/contrib/social_auth_hid/src/Controller/HidAuthController.php
+++ html/modules/contrib/social_auth_hid/src/Controller/HidAuthController.php
@@ -61,27 +61,72 @@ class HidAuthController extends OAuth2ControllerBase {
    * HID returns the user here after user has authenticated in HID.
    */
   public function callback() {
-    // Checks if authentication failed.
-    if ($this->request->getCurrentRequest()->query->has('error')) {
-      $this->messenger->addError($this->t('You could not be authenticated.'));
+    // Checks if there was an authentication error.
+    $response = $this->checkAuthError();
 
-      return $this->redirect('user.login');
+    // If there are no errors, attempt to authenticate the user.
+    if (!$response) {
+      /* @var \League\OAuth2\Client\Provider\Hid|null $profile */
+      $profile = $this->processCallback();
+
+      // If authentication was successful.
+      if ($profile !== NULL) {
+        // Gets (or not) extra initial data.
+        $data = $this->userAuthenticator->checkProviderIsAssociated($profile->getId()) ? NULL : $this->providerManager->getExtraDetails();
+
+        // If user information could be retrieved.
+        $response = $this->userAuthenticator->authenticateUser($profile->getName(), $profile->getEmail(), $profile->getId(), $this->providerManager->getAccessToken(), $profile->getAvatar(), $data);
+      }
     }
 
-    /* @var \League\OAuth2\Client\Provider\Hid|null $profile */
-    $profile = $this->processCallback();
-
-    // If authentication was successful.
-    if ($profile !== NULL) {
-
-      // Gets (or not) extra initial data.
-      $data = $this->userAuthenticator->checkProviderIsAssociated($profile->getId()) ? NULL : $this->providerManager->getExtraDetails();
-
-      // If user information could be retrieved.
-      return $this->userAuthenticator->authenticateUser($profile->getName(), $profile->getEmail(), $profile->getId(), $this->providerManager->getAccessToken(), $profile->getAvatar(), $data);
+    // Prevent redirect loop.
+    //
+    // This happens when `auto_redirect` is checked and a redirection to the
+    // user login form is returned by `OAuth2ControllerBase::checkAuthError()`
+    // or `userAuthenticator::authenticateUser()`.
+    //
+    // List of cases where a redirection to the login form occurs:
+    //
+    // - There is an authentication error on the provider side.
+    //
+    //   See OAuth2ControllerBase::checkAuthError().
+    //   See UserAuthenticator::dispatchAuthenticationError().
+    //
+    // - The provider could not be associated with the account (error).
+    //
+    //   See UserAuthenticator::associateNewProvider().
+    //
+    // - Trying to log in with the user 1 when authenticating with the admin
+    //   account is disabled.
+    //
+    //   See UserAuthenticator::authenticateExistingUser().
+    //
+    // - Trying to log in with an account that has a role for which logging in
+    //   with Social Auth is disabled.
+    //
+    //   See UserAuthenticator::authenticateExistingUser().
+    //
+    // - Trying to log in with an account that is blocked (not yet approved
+    //   for example).
+    //
+    //   See UserAuthenticator::authenticateExistingUser().
+    //
+    // - Logging in as a new user and admin approval is required.
+    //
+    //   See UserAuthenticator::authenticateNewUser().
+    //
+    // - Logging in as a new user and registration is disabled.
+    //
+    //   See UserAuthenticator::authenticateNewUser().
+    //
+    // @see \Drupal\social_auth\Controller\OAuth2ControllerBase::checkAuthError()
+    // @see \Drupal\social_auth\User\UserAuthenticator
+    // @see \Drupal\social_auth_hid\Routing\RouteSubscriber::alterRoutes()
+    if (!$response || $response->getTargetUrl() === '/user/login/hid') {
+      return $this->redirect('<front>');
     }
 
-    return $this->redirect('user.login');
+    return $response;
   }
 
 }
