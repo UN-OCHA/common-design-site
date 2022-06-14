8f7e7bf7aa5f77c0cf87a8c5890c3fa957512767
Peter Lieverdink <lieverdink@reliefweb.int>
2022-06-14 10:41:48 +1000

    fix: 🙄

--- html/themes/custom/common_design_subtheme/jest-puppeteer.config.js
+++ html/themes/custom/common_design_subtheme/jest-puppeteer.config.js
@@ -14,7 +14,7 @@ const DEBUG_MODE = process.argv.includes('--debug');
 
 // When debugging we want to see the browser do its work.
 if (DEBUG_MODE) {
-  config.launch.headless = FALSe;
+  config.launch.headless = FALSE;
   config.testTimeout = 10000;
   config.launch.slowMo = 100;
 }
