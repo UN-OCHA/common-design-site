a512e16ec5ceb990e9cb3cfeb6e90727b9842613
left23 <lcdemc@gmail.com>
2023-11-23 11:52:26 +0100

    chore: urls plural
    Refs: CD-511

--- .github/workflows/lighthouserc.js
+++ .github/workflows/lighthouserc.js
@@ -1,6 +1,6 @@
 module.exports = {
   ci: {
-    url: ["http://localhost/", "http://localhost/demo,", "http://127.0.0.1:8080", "http://127.0.0.1:8080/demo"],
+    urls: ["http://localhost/", "http://localhost/demo,", "http://127.0.0.1:8080", "http://127.0.0.1:8080/demo"],
     assert: {
       preset: "lighthouse:recommended",
       assertions: {
