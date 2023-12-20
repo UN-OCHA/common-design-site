0ce25b2c75e93632db555e6f2da9698e8d95239f
left23 <lcdemc@gmail.com>
2023-12-20 15:56:20 +0100

    chore: URLs are in the test file
    Refs: CD-511

--- .github/workflows/lighthouserc.js
+++ .github/workflows/lighthouserc.js
@@ -1,11 +1,6 @@
 module.exports = {
   ci: {
     collect: {
-      url: [
-        'http://127.0.0.1:8080',
-        'http://127.0.0.1:8080/demo',
-        'http://127.0.0.1:8080/layouts/sidebar-first'
-      ],
       startServerCommand: 'docker compose -f tests/docker-compose.yml exec -T drupal drush rs 127.0.0.1:8080'
     },
     assert: {
