2480e82a10f0b504d495aa322e08b7418af8cd75
Peter Droogmans (attiks) <peter@attiks.com>
2024-04-05 15:11:31 +0200

    feat: Register link
    Refs: #OPS-7872

---
+++ html/modules/custom/azure_tweaks/src/Controller/AuthController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Drupal\azure_tweaks\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Routing\TrustedRedirectResponse;
+use Drupal\Core\Url;
+
+/**
+ * Returns responses for Social Auth Hid module routes.
+ */
+class AuthController extends ControllerBase {
+
+  public function redirectRegister() {
+    $url = $this->config('azure_tweaks.settings')->get('register_url');
+    $client_id = $this->config('openid_connect.client.uniteid')->get('settings.client_id');
+    $redirect = Url::fromRoute('<front>')->setAbsolute()->toString();
+
+    $url .= '&client_id=' . $client_id;
+    $url .= '&redirect_uri=' . $redirect;
+
+    /** @var \Drupal\Core\Routing\TrustedRedirectResponse|\Symfony\Component\HttpFoundation\RedirectResponse $response */
+    $response = new TrustedRedirectResponse($url);
+
+    return $response->send();
+  }
+
+  public function redirectResetPassword() {
+    $config = $this->config('azure_tweaks.settings');
+
+    /** @var \Drupal\Core\Routing\TrustedRedirectResponse|\Symfony\Component\HttpFoundation\RedirectResponse $response */
+    $response = new TrustedRedirectResponse($config->get('password_url'));
+
+    return $response->send();
+  }
+
+}
